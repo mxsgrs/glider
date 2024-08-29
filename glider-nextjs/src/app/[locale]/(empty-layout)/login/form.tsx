4a516d8073f7d558554cc690887692f0db07cfea
Maxime Gros <mxs.grs@gmail.com>
2024-08-29 11:13:26 +0200

    Login page translation

--- glider-nextjs/src/app/[locale]/(empty-layout)/login/form.tsx
+++ glider-nextjs/src/app/[locale]/(empty-layout)/login/form.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import {useTranslations} from 'next-intl'
-import { Bebas_Neue } from "next/font/google"
-import { useRouter } from "next/navigation"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { useTranslations } from 'next-intl';
+import { Bebas_Neue } from 'next/font/google';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import {
   Form,
   FormControl,
@@ -16,42 +16,42 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Checkbox } from "@/components/ui/checkbox"
+} from '@/components/ui/form';
+import { Checkbox } from '@/components/ui/checkbox';
 
 const formSchema = z.object({
   username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+    message: 'Username must be at least 2 characters.',
   }),
   password: z.string().min(8, {
-    message: "Password must be at least 8 characters.",
+    message: 'Password must be at least 8 characters.',
   }),
   rememberMe: z.boolean(),
 });
 
 const bebasNeue = Bebas_Neue({
-  weight: "400",
-  style: "normal",
-  subsets: ["latin"],
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
 });
 
 export default function LoginForm() {
-  const t = useTranslations('Login');
-  type FormValues = z.infer<typeof formSchema>
+  const t = useTranslations('login');
+  type FormValues = z.infer<typeof formSchema>;
 
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "demimo",
-      password: "K3oCs97e",
+      username: 'demimo',
+      password: 'K3oCs97e',
       rememberMe: true,
     },
-  })
+  });
 
   const router = useRouter();
 
   async function onSubmit(values: FormValues) {
-    router.push("/");
+    router.push('/');
   }
 
   return (
@@ -67,9 +67,9 @@ export default function LoginForm() {
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="pl-2">Username</FormLabel>
+                <FormLabel className="pl-2">{t('username')}</FormLabel>
                 <FormControl>
-                  <Input placeholder="Username" {...field} />
+                  <Input placeholder={t('username')} {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -80,9 +80,9 @@ export default function LoginForm() {
             name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="pl-2">Password</FormLabel>
+                <FormLabel className="pl-2">{t('password')}</FormLabel>
                 <FormControl>
-                  <Input type="password" placeholder="Password" {...field} />
+                  <Input type="password" placeholder={t('password')} {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -100,7 +100,7 @@ export default function LoginForm() {
                   />
                 </FormControl>
                 <FormLabel className="ml-2 text-muted-foreground">
-                  Remember me
+                  {t('rememberMe')}
                 </FormLabel>
                 <FormMessage />
               </FormItem>
@@ -108,9 +108,9 @@ export default function LoginForm() {
           />
         </div>
         <Button type="submit" className="w-full mt-8">
-          Submit
+          {t('submit')}
         </Button>
       </form>
     </Form>
-  )
+  );
 }
