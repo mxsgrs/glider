c8bf71a669e5ad2ce8871a64a577c2d48a8d6374
Maxime Gros <mxs.grs@gmail.com>
2024-08-30 22:21:46 +0200

    Profile page

---
+++ glider-nextjs/src/app/[locale]/(standard-layout)/profile/page.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "@/components/ui/page-header"
+
+export default function Page() {
+    return (
+        <div>
+            <PageHeader title="Profile information" subtitle="Your personal information on this app." />
+        </div>
+    )
+}
