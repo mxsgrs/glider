a4606a360ab3245c8a08405ca17880d6942d2264
Maxime Gros <mxs.grs@gmail.com>
2024-09-01 11:38:17 +0200

    Estimate only in sitemap.xml

--- glider-nextjs/src/app/sitemap.tsx
+++ glider-nextjs/src/app/sitemap.tsx
@@ -3,26 +3,14 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://glider-logistics.com/dashboard',
+      url: 'https://glider-logistics.com/estimate',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
       alternates: {
         languages: {
-          en: 'https://glider-logistics.com/en/dashboard',
-          fr: 'https://glider-logistics.com/fr/dashboard',
-        },
-      },
-    },
-    {
-      url: 'https://glider-logistics.com/login',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-      alternates: {
-        languages: {
-          en: 'https://glider-logistics.com/en/login',
-          fr: 'https://glider-logistics.com/fr/login',
+          en: 'https://glider-logistics.com/en/estimate',
+          fr: 'https://glider-logistics.com/fr/estimate',
         },
       },
     },
