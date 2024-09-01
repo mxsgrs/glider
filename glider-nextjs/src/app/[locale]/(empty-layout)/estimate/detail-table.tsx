b0f8c402d043ac01a1c65db15b1e6cf2c9ca9605
Maxime Gros <mxs.grs@gmail.com>
2024-09-01 14:19:47 +0200

    Table for estimate details

---
+++ glider-nextjs/src/app/[locale]/(empty-layout)/estimate/detail-table.tsx
@@ -0,0 +1,65 @@
+import { EstimateDetail } from '@/types/estimate';
+import { StyleSheet, Text, View } from '@react-pdf/renderer'
+import PropTypes from 'prop-types'
+
+type DetailTableProps = {
+    data: EstimateDetail[];
+};
+
+export const DetailTable: React.FC<DetailTableProps> = ({ data }) => {
+    return (
+        <View style={styles.table}>
+            <View style={[styles.row, styles.bold, styles.header]}>
+                <Text style={styles.col1}>Description</Text>
+                <Text style={styles.col2}>Quantity</Text>
+                <Text style={styles.col3}>Unit Price</Text>
+                <Text style={styles.col4}>Total</Text>
+            </View>
+            {data.map((row) => (
+                <View key={row.estimateDetailId} style={styles.row} wrap={false}>
+                    <Text style={styles.col1}>{row.rawDescription}</Text>
+                    <Text style={styles.col2}>{row.quantity}</Text>
+                    <Text style={styles.col3}>{row.unitPrice.toFixed(2)}</Text>
+                    <Text style={styles.col4}>{(row.quantity*row.unitPrice).toFixed(2)}</Text>
+                </View>
+            ))}
+        </View>
+    );
+};
+
+DetailTable.propTypes = {
+    data: PropTypes.array.isRequired,
+}
+
+const styles = StyleSheet.create({
+    table: {
+        width: '100%',
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        borderTop: '1px solid #EEE',
+        paddingTop: 8,
+        paddingBottom: 8,
+    },
+    header: {
+        borderTop: 'none',
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+    col1: {
+        width: '55%',
+    },
+    col2: {
+        width: '15%',
+    },
+    col3: {
+        width: '15%',
+    },
+    col4: {
+        width: '15%',
+    }
+})
+
+export default DetailTable
--- glider-nextjs/src/app/[locale]/(empty-layout)/estimate/estimate-pdf.tsx
+++ glider-nextjs/src/app/[locale]/(empty-layout)/estimate/estimate-pdf.tsx
@@ -3,6 +3,46 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet, Font, Image } from '@react-pdf/renderer';
 import { Estimate } from '@/types/estimate';
+import ReportTable from './detail-table';
+
+interface EstimatePdfProps {
+    estimate: Estimate;
+}
+
+export const EstimatePdf: React.FC<EstimatePdfProps> = ({ estimate }) => {
+    return (
+        <Document>
+            <Page style={styles.page}>
+                <View style={styles.top}>
+                    <Image src="/images/urban-home.jpg" style={styles.logo} />
+                    <View style={styles.section}>
+                        <Text style={styles.pageTitle}>Estimate #{estimate.estimateId}</Text>
+                        <View style={styles.estimateMetadatas}>
+                            <Text style={styles.estimateMetadata}>Subject Matter: {estimate.subjectMatter}</Text>
+                            <Text style={styles.estimateMetadata}>Date of issue: {estimate.creationDate?.toLocaleDateString()}</Text>
+                            <Text style={styles.estimateMetadata}>Expiry date: {estimate.creationDate?.toLocaleDateString()}</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.companies}>
+                    {estimate.estimateCompany.map((company) => (
+                        <View key={company.estimateCompanyId} style={styles.company}>
+                            <Text style={styles.header}>{company.estimateCompanyParty}</Text>
+                            <Text style={styles.companyDetail}>{company.businessName}</Text>
+                            <Text style={styles.companyDetail}>{company.businessAddress}</Text>
+                            <Text style={styles.companyDetail}>{company.phone}</Text>
+                            <Text style={styles.companyDetail}>{company.email}</Text>
+                        </View>
+                    ))}
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.header}>Details</Text>
+                    <ReportTable data={estimate.estimateDetail} />
+                </View>
+            </Page>
+        </Document>
+    );
+};
 
 Font.register({
     family: 'Open Sans',
@@ -18,11 +58,18 @@ const styles = StyleSheet.create({
         fontFamily: 'Open Sans',
         padding: 20,
     },
+    estimateMetadatas: {
+        marginTop: 10,
+    },
+    estimateMetadata: {
+        marginTop: 5,
+    },
     top: {
         display: 'flex',
         flexDirection: 'row',
-        margin: 10,
+        gap: 20,
         padding: 10,
+        margin: 10,
     },
     logo: {
         width: 120,
@@ -41,10 +88,17 @@ const styles = StyleSheet.create({
         width: '50%',
         padding: 10,
     },
+    companyDetail: {
+        marginTop: 5,
+    },
     header: {
         fontSize: 12,
         fontWeight: 'bold',
         marginBottom: 5,
+    },
+    pageTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
     },
     table: {
         display: 'flex',
@@ -53,10 +107,8 @@ const styles = StyleSheet.create({
         borderColor: '#000',
         borderWidth: 1,
         borderCollapse: 'collapse',
-        marginBottom: 10,
     },
     tableRow: {
-        margin: 'auto',
         flexDirection: 'row',
     },
     tableCol: {
@@ -73,57 +125,3 @@ const styles = StyleSheet.create({
         fontSize: 12,
     },
 });
-
-interface EstimatePdfProps {
-    estimate: Estimate;
-}
-
-export const EstimatePdf: React.FC<EstimatePdfProps> = ({ estimate }) => {
-    return (
-        <Document>
-            <Page style={styles.page}>
-                <View style={styles.top}>
-                    <Image src="/images/urban-home.jpg" style={styles.logo} />
-                    <View style={styles.section}>
-                        <Text style={styles.header}>Estimate #{estimate.estimateId}</Text>
-                        <Text>Subject Matter: {estimate.subjectMatter}</Text>
-                        <Text>Created On: {estimate.creationDate?.toLocaleDateString()}</Text>
-                        <Text>Updated On: {estimate.updateDate?.toLocaleDateString()}</Text>
-                    </View>
-                </View>
-                <View style={styles.companies}>
-                    {estimate.estimateCompany.map((company) => (
-                        <View key={company.estimateCompanyId} style={styles.company}>
-                            <Text style={styles.header}>{company.estimateCompanyParty}</Text>
-                            <Text>{company.businessName}</Text>
-                            <Text>{company.businessAddress}</Text>
-                            <Text>{company.phone}</Text>
-                            <Text>{company.email}</Text>
-                        </View>
-                    ))}
-                </View>
-                <View style={styles.section}>
-                    <Text style={styles.header}>Details</Text>
-                    <View style={styles.table}>
-                        <View style={styles.tableRow}>
-                            <Text style={styles.tableCol}>Description</Text>
-                            <Text style={styles.tableCol}>Quantity</Text>
-                            <Text style={styles.tableCol}>Unit Price</Text>
-                            <Text style={styles.tableCol}>Total</Text>
-                        </View>
-                        {estimate.estimateDetail.map((detail) => (
-                            <View key={detail.estimateDetailId} style={styles.tableRow}>
-                                <Text style={styles.tableCell}>{detail.rawDescription}</Text>
-                                <Text style={styles.tableCell}>{detail.quantity}</Text>
-                                <Text style={styles.tableCell}>{detail.unitPrice.toFixed(2)}</Text>
-                                <Text style={styles.tableCell}>
-                                    {(detail.quantity * detail.unitPrice).toFixed(2)}
-                                </Text>
-                            </View>
-                        ))}
-                    </View>
-                </View>
-            </Page>
-        </Document>
-    );
-};
